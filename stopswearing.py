71ee7ae48a64df3d23aa42d858cbcc25db2def8e
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-05-25 11:00:38 +1000

    Update stopswearing.py

--- stopswearing.py
+++ stopswearing.py
@@ -1,17 +1,35 @@
-from pocketsphinx import LiveSpeech, get_model_path
-import pyttsx3
+#!/usr/bin/python
 
-engine = pyttsx3.init()
-model_path = get_model_path()
-speech = LiveSpeech(
-    verbose=False,
-    sampling_rate=16000,
-    buffer_size=2048,
-    no_search=False,
-    full_utt=False,
-    hmm=os.path.join(model_path, 'en-us'),
-    lm=os.path.join(model_path, 'en-us.lm.bin'),
-    dic=os.path.join(model_path, 'cmudict-en-us.dict')
-)
-for phrase in speech:
-    print(phrase)
+from os import environ, path
+
+from pocketsphinx import *
+from sphinxbase import *
+
+MODELDIR = "../../../model"
+DATADIR = "../../../test/data"
+
+config = Decoder.default_config()
+config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+config.set_string('-logfn', '/dev/null')
+decoder = Decoder(config)
+
+stream = open(path.join(DATADIR, 'goforward.raw'), 'rb')
+#stream = open('10001-90210-01803.wav', 'rb')
+
+in_speech_bf = False
+decoder.start_utt()
+while True:
+    buf = stream.read(1024)
+    if buf:
+        decoder.process_raw(buf, False, False)
+        if decoder.get_in_speech() != in_speech_bf:
+            in_speech_bf = decoder.get_in_speech()
+            if not in_speech_bf:
+                decoder.end_utt()
+                print 'Result:', decoder.hyp().hypstr
+                decoder.start_utt()
+    else:
+        break
+decoder.end_utt()
