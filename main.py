4a61abf2708f729c91e6c30fe781b1d69f4f82cf
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-05-25 23:42:18 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source)
-try: ##this is the sphinx recognition part
-    print(r.recognize_sphinx(audio))
+AZURE_SPEECH_KEY = "INSERT AZURE SPEECH API KEY HERE"  # Microsoft Speech API keys 32-character lowercase hexadecimal strings
+try:
+    print("Microsoft Azure Speech thinks you said " + r.recognize_azure(audio, key=AZURE_SPEECH_KEY))
 except sr.UnknownValueError:
-    print("???")
+    print("Microsoft Azure Speech could not understand audio")
 except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+    print("Could not request results from Microsoft Azure Speech service; {0}".format(e))
