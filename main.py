8284a4174f9c31de935648695fab76a67fe3e27b
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-05-26 09:49:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ with sr.Microphone() as source:
     audio = r.listen(source)
 AZURE_SPEECH_KEY = "INSERT AZURE SPEECH API KEY HERE"  # Microsoft Speech API keys 32-character lowercase hexadecimal strings
 try:
-    print("Microsoft Azure Speech thinks you said " + r.recognize_azure(audio, key=AZURE_SPEECH_KEY))
+    print(r.recognize_azure(audio, key=AZURE_SPEECH_KEY))
 except sr.UnknownValueError:
-    print("Microsoft Azure Speech could not understand audio")
+    print("???")
 except sr.RequestError as e:
     print("Could not request results from Microsoft Azure Speech service; {0}".format(e))
