1326c6415b83a70467d1f9c7aea645516ca70d42
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-05-26 11:53:01 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import speech_recognition as sr
 r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source)
-AZURE_SPEECH_KEY = "INSERT AZURE SPEECH API KEY HERE"  # Microsoft Speech API keys 32-character lowercase hexadecimal strings
+IBM_USERNAME = "jack.stanton@trac.nsw.edu.au"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+IBM_PASSWORD = "Jack0808"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
 try:
-    print(r.recognize_azure(audio, key=AZURE_SPEECH_KEY))
+    print(r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
 except sr.UnknownValueError:
-    print("???")
+    print("IBM Speech to Text could not understand audio")
 except sr.RequestError as e:
-    print("Could not request results from Microsoft Azure Speech service; {0}".format(e))
+    print("Could not request results from IBM Speech to Text service; {0}".format(e))
