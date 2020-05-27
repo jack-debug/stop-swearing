f376f94707d257c21284eb54bbd2dc17d0f79faf
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-05-27 10:10:41 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import speech_recognition as sr
 r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source)
-IBM_USERNAME = "jack.stanton@trac.nsw.edu.au"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-IBM_PASSWORD = "Jack0808"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
 try:
-    print(r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
+    print(r.recognize_google(audio))
 except sr.UnknownValueError:
-    print("IBM Speech to Text could not understand audio")
+    print("Error: Audio could not be understood")
 except sr.RequestError as e:
-    print("Could not request results from IBM Speech to Text service; {0}".format(e))
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
